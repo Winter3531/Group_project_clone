0661ecdca17f3afaff6ed42b9ee8103089285bea
Winter3531 <winterdavid31@gmail.com>
2023-05-07 16:48:49 -0600

    working on song modals

--- react-app/src/components/Songs/SongAddModal.js
+++ react-app/src/components/Songs/SongAddModal.js
@@ -3,10 +3,31 @@ import { useDispatch, useSelector } from "react-redux"
 import { useModal } from "../../context/Modal";
 
 export default function SongAddModal(){
+    const dispatch = useDispatch();
 
     return (
         <>
-            <p>Add Here</p>
+            <h1 className="new-song-modal-header">Add New Song</h1>
+            <form className="new-song-modal-form"  >
+                {/* <input
+                    type="text"
+                    value={song_name}
+                    onChange={(e) => setSongName(e.target.value)}
+                    placeholder={song_name}
+                />
+                <input
+                    type="text"
+                    value={song_src}
+                    onChange={(e) => setSongName(e.target.value)}
+                    placeholder={song_src}
+                />
+                <input
+                    type="text"
+                    value={song_length}
+                    onChange={(e) => setSongName(e.target.value)}
+                    placeholder={song_length}
+                /> */}
+            </form>
         </>
     )
 }
